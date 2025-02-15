6adc50243999384831ff4244c5c8cb67709f7887
exxyeon <leehayeon200@naver.com>
2025-02-15 18:56:55 +0900

    Feat: 소셜 로그인 기본 구조 구현
    - OAuth2 로그인 기본 구조 설정 (Google, Naver, Kakao)
    - Security 및 OAuth2 관련 설정 추가 (application.yml, SecurityConfig)
    - OAuth2UserService 및 관련 DTO 구현
    - JWT 및 인증 관련 의존성 추가 (Spring Security, jjwt)

---
+++ roome/src/main/java/com/roome/domain/oauth2/dto/NaverResponse.java
@@ -0,0 +1,32 @@
+package com.roome.domain.oauth2.dto;
+
+import java.util.Map;
+
+public class NaverResponse implements OAuth2Response {
+
+    private final Map<String, Object> attributes;
+
+    public NaverResponse(Map<String, Object> attributes) {
+        this.attributes = (Map<String, Object>) attributes.get("response");
+    }
+
+    @Override
+    public OAuth2Provider getProvider() {
+        return OAuth2Provider.NAVER;
+    }
+
+    @Override
+    public String getProviderId() {
+        return attributes.get("id").toString();
+    }
+
+    @Override
+    public String getName() {
+        return attributes.get("name").toString();
+    }
+
+    @Override
+    public String getProfileImageUrl() {
+        return attributes.get("profile_image").toString();
+    }
+}
