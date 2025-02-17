53c20740c116cdc2b73c5aa6b0acdd85916b1f3e
daeyoung <pstor23@gmail.com>
2025-02-17 22:44:12 +0900

    Feat: 등록 도서 목록 조회, 전체 등록 도서 개수 조회 기능
    - feat: MyBookController -> 등록 도서 목록 조회 api 추가
    
    - feat: MyBookRepository -> 무한 스크롤 방식으로 첫 번째 페이지 조회, 두 번째 ~ 마지막 페이지 조회 쿼리 작성
    
    - feat: MyBookService -> 등록 도서 목록 (페이징), 전체 등록 도서 개수를 response로 응답

--- roome/src/main/java/com/roome/domain/mybook/entity/repository/MyBookRepository.java
+++ roome/src/main/java/com/roome/domain/mybook/entity/repository/MyBookRepository.java
@@ -2,6 +2,39 @@ package com.roome.domain.mybook.entity.repository;
 
 import com.roome.domain.mybook.entity.MyBook;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface MyBookRepository extends JpaRepository<MyBook, Long> {
+
+    @Query(
+            value = """
+                    select mb
+                    from MyBook mb
+                    join fetch mb.book
+                    where mb.room.id = :roomId
+                    order by mb.id desc limit :limit
+                    """
+    )
+    List<MyBook> findAll(
+            @Param("roomId") Long roomId,
+            @Param("limit") Long limit
+    );
+
+    @Query(
+            value = """
+                    select mb
+                    from MyBook mb
+                    join fetch mb.book
+                    where mb.room.id = :roomId and mb.id < :lastMyBookId
+                    order by mb.id desc limit :limit
+                    """
+    )
+    List<MyBook> findAll(
+            @Param("roomId") Long roomId,
+            @Param("limit") Long limit,
+            @Param("lastMyBookId") Long lastMyBookId
+    );
 }
