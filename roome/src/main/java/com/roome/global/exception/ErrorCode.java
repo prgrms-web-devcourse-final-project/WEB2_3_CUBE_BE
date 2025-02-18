77a9bcc15139484910c10e30f3caeaa30e2abebb
soyeon1806 <ksy1118s@naver.com>
2025-02-18 14:43:45 +0900

    Feat: room theme 관련 에러 코드 추가

--- roome/src/main/java/com/roome/global/exception/ErrorCode.java
+++ roome/src/main/java/com/roome/global/exception/ErrorCode.java
@@ -24,8 +24,8 @@ public enum ErrorCode {
   USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
 
   // Room 관련 예외
-  ROOM_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 방을 찾을 수 없습니다.")
-
+  ROOM_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 방을 찾을 수 없습니다."),
+  INVALID_ROOM_THEME(HttpStatus.BAD_REQUEST, "해당 테마가 유효하지 않거나 입력 값이 비어 있습니다.")
   ;
 
   private final HttpStatus status;
