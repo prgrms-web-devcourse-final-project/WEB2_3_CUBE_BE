b78b30c0653ff1ef4c0dd584ef4ecba5a1e8c9ec
Jhynguu <jhyngu0531@gmail.com>
2025-02-19 07:05:55 +0900

    feat: CD 추가 요청 및 응답 DTO 추가
    - `MyCdRequest` 추가: CD 추가 요청 시 필요한 데이터 포함
    - `MyCdResponse` 추가: CD 추가 응답 시 반환할 데이터 포함

---
+++ roome/src/main/java/com/roome/domain/mycd/dto/MyCdRequest.java
@@ -0,0 +1,37 @@
+package com.roome.domain.mycd.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MyCdRequest {
+
+  @NotNull
+  private Long cdId;
+
+  @NotBlank
+  private String title;
+
+  @NotBlank
+  private String artist;
+
+  @NotBlank
+  private String album;
+
+  @NotBlank
+  private String genre;
+
+  @NotBlank
+  private String coverUrl;
+
+  @NotBlank
+  private String youtubeUrl;
+
+  @NotNull
+  private int duration;
+}
