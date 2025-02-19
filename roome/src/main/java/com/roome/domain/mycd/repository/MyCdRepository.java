d1c418d1a57f1f73a361ddaa0fea284e09036eb0
Jhynguu <jhyngu0531@gmail.com>
2025-02-19 19:57:49 +0900

    feat: MyCdRepository findByIdAndUserId() 메서드 추가

--- roome/src/main/java/com/roome/domain/mycd/repository/MyCdRepository.java
+++ roome/src/main/java/com/roome/domain/mycd/repository/MyCdRepository.java
@@ -2,6 +2,7 @@ package com.roome.domain.mycd.repository;
 
 import com.roome.domain.mycd.entity.MyCd;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MyCdRepository extends JpaRepository<MyCd, Long> {
@@ -9,4 +10,7 @@ public interface MyCdRepository extends JpaRepository<MyCd, Long> {
   boolean existsByUserIdAndCdId(Long userId, Long cdId);
 
   List<MyCd> findByUserId(Long userId);
+
+  Optional<MyCd> findByIdAndUserId(Long myCdId, Long userId);
+
 }
