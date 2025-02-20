2c9b73473767d6f140d2d268000245780adfb0ec
daeyoung <pstor23@gmail.com>
2025-02-20 11:28:00 +0900

    Feat: 도서 정보를 저장하기 전에 isbn으로 DB에 동일한 도서가 있는지 확인

--- roome/src/main/java/com/roome/domain/book/entity/repository/BookRepository.java
+++ roome/src/main/java/com/roome/domain/book/entity/repository/BookRepository.java
@@ -3,5 +3,9 @@ package com.roome.domain.book.entity.repository;
 import com.roome.domain.book.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BookRepository extends JpaRepository<Book, Long> {
+
+    Optional<Book> findByIsbn(Long isbn);
 }
