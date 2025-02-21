145ca596c50a49259dea613d3cdfe59d6e51e957
Jhynguu <jhyngu0531@gmail.com>
2025-02-21 12:33:02 +0900

    refactor: CdComment Request, Response DTO 수정

--- roome/src/main/java/com/roome/domain/cdcomment/dto/CdCommentResponse.java
+++ roome/src/main/java/com/roome/domain/cdcomment/dto/CdCommentResponse.java
@@ -8,11 +8,10 @@ import java.time.LocalDateTime;
 @Getter
 @AllArgsConstructor
 public class CdCommentResponse {
-  private Long commentId;  // 댓글 ID
-  private Long myCdId;  // CD ID
-  private Long authorId;  // 작성자 ID
-  private String nickname;  // 작성자 닉네임
-  private Long timestampSec;  // 타임스탬프 (초) (선택 사항)
-  private String content;  // 댓글 내용
-  private LocalDateTime createdAt;  // 작성 날짜
+  private Long id;
+  private Long myCdId;
+  private Long userId;
+  private String timestamp;
+  private String content;
+  private LocalDateTime createdAt;
 }
