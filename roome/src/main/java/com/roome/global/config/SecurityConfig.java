eb9512c99df55951c26d09387b3d56de3adba44d
exxyeon <leehayeon200@naver.com>
2025-02-23 00:39:23 +0900

    Chore: 보안 및 Swagger 설정 수정

--- roome/src/main/java/com/roome/global/config/SecurityConfig.java
+++ roome/src/main/java/com/roome/global/config/SecurityConfig.java
@@ -1,9 +1,6 @@
 package com.roome.global.config;
 
-import com.roome.domain.auth.service.CustomOAuth2UserService;
 import com.roome.global.jwt.filter.JwtAuthenticationFilter;
-import com.roome.global.jwt.handler.OAuth2FailureHandler;
-import com.roome.global.jwt.handler.OAuth2SuccessHandler;
 import com.roome.global.jwt.service.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -11,9 +8,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.oauth2.client.web.AuthorizationRequestRepository;
-import org.springframework.security.oauth2.client.web.HttpSessionOAuth2AuthorizationRequestRepository;
-import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -23,50 +17,35 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig {
 
     private final JwtTokenProvider jwtTokenProvider;
-    private final CustomOAuth2UserService customOAuth2UserService;
-    private final OAuth2SuccessHandler oAuth2SuccessHandler;
-    private final OAuth2FailureHandler oAuth2FailureHandler;
-
-    @Bean
-    public AuthorizationRequestRepository<OAuth2AuthorizationRequest> authorizationRequestRepository() {
-        return new HttpSessionOAuth2AuthorizationRequestRepository();
-    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-            // CSRF 보호 비활성화
-            .csrf((auth) -> auth.disable())
 
-            // 폼 로그인, HTTP 기본 인증 비활성화
-            .formLogin((form) -> form.disable())
-            .httpBasic((auth) -> auth.disable())
-            .sessionManagement((session) -> session
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                // CSRF 보호 비활성화
+                .csrf(csrf -> csrf.disable())
 
-            // OAuth2 로그인 설정
-            .oauth2Login(oauth2 -> oauth2
-                .authorizationEndpoint(endpoint -> endpoint
-                    .baseUri("/oauth2/authorization")
-                    .authorizationRequestRepository(authorizationRequestRepository()))
-                .redirectionEndpoint(redirection -> redirection
-                    .baseUri("/login/oauth2/code/*"))
-                .userInfoEndpoint(userInfo -> userInfo
-                    .userService(customOAuth2UserService))
-                .successHandler(oAuth2SuccessHandler)
-                .failureHandler(oAuth2FailureHandler))
+                // 폼 로그인, HTTP 기본 인증 비활성화
+                .formLogin((form) -> form.disable())
+                .httpBasic((auth) -> auth.disable())
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 
-            // 접근 제어 설정
-            .authorizeHttpRequests((auth) -> auth
-                .requestMatchers(
-                    "/**"
-                ).permitAll()
-                .anyRequest().authenticated());
+                // 접근 제어 설정
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers(
+                                "/api/auth/**",
+                                "/error",
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/mock/**"
+                        ).permitAll()
+                        .anyRequest().authenticated());
 
         http.addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
-            UsernamePasswordAuthenticationFilter.class);
+                UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
-
 }
