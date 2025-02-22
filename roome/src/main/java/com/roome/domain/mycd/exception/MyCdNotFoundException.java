9c1746fd3574e238f02a8bd7df1f3a8d1517453f
Jhynguu <jhyngu0531@gmail.com>
2025-02-22 22:25:05 +0900

    refactor: MyCd Exception 수정

--- roome/src/main/java/com/roome/domain/mycd/exception/MyCdNotFoundException.java
+++ roome/src/main/java/com/roome/domain/mycd/exception/MyCdNotFoundException.java
@@ -3,9 +3,8 @@ package com.roome.domain.mycd.exception;
 import com.roome.global.exception.BusinessException;
 import com.roome.global.exception.ErrorCode;
 
-public class DuplicateCdException extends BusinessException {
-
-  public DuplicateCdException(Long cdId) {
-    super(ErrorCode.DUPLICATE_CD);
+public class MyCdNotFoundException extends BusinessException {
+  public MyCdNotFoundException() {
+    super(ErrorCode.MYCD_NOT_FOUND);
   }
 }
