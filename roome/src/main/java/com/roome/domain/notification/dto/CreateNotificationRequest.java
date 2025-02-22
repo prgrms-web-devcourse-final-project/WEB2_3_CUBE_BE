353b78e34afaab94b24407d9e3c96341701f2e8b
chesthyeon <niw927@naver.com>
2025-02-22 17:58:00 +0900

    Feat 알림 : mock dto 추가

--- roome/src/main/java/com/roome/domain/notification/dto/CreateNotificationRequest.java
+++ roome/src/main/java/com/roome/domain/notification/dto/CreateNotificationRequest.java
@@ -1,10 +1,12 @@
 package com.roome.domain.notification.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 public class CreateNotificationRequest {
     private Long senderId;
     private Long targetId;
--- roome/src/main/java/com/roome/domain/notification/dto/CreateNotificationResponse.java
+++ roome/src/main/java/com/roome/domain/notification/dto/CreateNotificationResponse.java
@@ -1,5 +1,13 @@
 package com.roome.domain.notification.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
 public class CreateNotificationResponse {
-    Long notificationId;
+    @NotNull
+    private Long notificationId;
 }
