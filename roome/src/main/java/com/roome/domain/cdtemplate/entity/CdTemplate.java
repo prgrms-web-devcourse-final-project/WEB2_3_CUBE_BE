1fc66134ec37bad10bdf2cbf6aac668c7fba1bac
Jhynguu <jhyngu0531@gmail.com>
2025-02-22 17:28:58 +0900

    refactor: CdTemplate Entity Class 수정

--- roome/src/main/java/com/roome/domain/cdtemplate/entity/CdTemplate.java
+++ roome/src/main/java/com/roome/domain/cdtemplate/entity/CdTemplate.java
@@ -3,14 +3,12 @@ package com.roome.domain.cdtemplate.entity;
 import com.roome.domain.mycd.entity.MyCd;
 import com.roome.global.entity.BaseEntity;
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Getter
 @Entity
-@Table(name = "cd_template")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "cd_template")
 public class CdTemplate extends BaseEntity {
 
   @Id
@@ -18,21 +16,22 @@ public class CdTemplate extends BaseEntity {
   private Long id;
 
   @OneToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "my_cd_id", nullable = false)
-  private MyCd myCd; // 템플릿이 달린 CD
+  @JoinColumn(name = "my_cd_id", nullable = false, unique = true)
+  private MyCd myCd;
 
-  @Column(nullable = false)
+  @Column(nullable = false, length = 255)
   private String comment1;
 
-  @Column(nullable = false)
+  @Column(nullable = false, length = 255)
   private String comment2;
 
-  @Column(nullable = false)
+  @Column(nullable = false, length = 255)
   private String comment3;
 
-  @Column(nullable = false)
+  @Column(nullable = false, length = 255)
   private String comment4;
 
+  @Builder
   public CdTemplate(MyCd myCd, String comment1, String comment2, String comment3, String comment4) {
     this.myCd = myCd;
     this.comment1 = comment1;
@@ -41,10 +40,10 @@ public class CdTemplate extends BaseEntity {
     this.comment4 = comment4;
   }
 
-  public void updateTemplate(String comment1, String comment2, String comment3, String comment4) {
-    if (comment1 != null) this.comment1 = comment1;
-    if (comment2 != null) this.comment2 = comment2;
-    if (comment3 != null) this.comment3 = comment3;
-    if (comment4 != null) this.comment4 = comment4;
+  public void update(String comment1, String comment2, String comment3, String comment4) {
+    this.comment1 = comment1;
+    this.comment2 = comment2;
+    this.comment3 = comment3;
+    this.comment4 = comment4;
   }
 }
