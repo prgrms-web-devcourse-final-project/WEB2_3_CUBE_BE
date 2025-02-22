b98077934d3bd1c79da29afd8c12a1ff00590805
chesthyeon <niw927@naver.com>
2025-02-22 16:54:28 +0900

    Refactor 알림 : Notification.java 엔티티 수정 유정 정보는 유저 테이블에서 꺼내오는 방식

--- roome/src/main/java/com/roome/domain/notification/entity/Notification.java
+++ roome/src/main/java/com/roome/domain/notification/entity/Notification.java
@@ -25,23 +25,14 @@ public class Notification extends BaseTimeEntity {
     private NotificationType type;
 
     @Column(nullable = false)
-    private String title;
-
-    @Column(nullable = false)
     private String content;
-    //보낸 사람의 user_id
+
     @Column(name = "sender_id", nullable = false)
     private Long senderId;
-    //보낸 사람의 이름
-    @Column(name = "sender_name", nullable = false)
-    private String senderName;
 
-    @Column(name = "sender_profile_image")
-    private String senderProfileImage;
-    //받는 사람의 target_id(음악, 이벤트, 게스트북 등의 id)
     @Column(name = "target_id", nullable = false)
     private Long targetId;
-    //받는 사람의 user_id
+
     @Column(name = "receiver_id", nullable = false)
     private Long receiverId;
 
@@ -50,14 +41,10 @@ public class Notification extends BaseTimeEntity {
 
     @Builder
     public Notification(NotificationType type, String title, String content,
-                        Long senderId, String senderName, String senderProfileImage,
-                        Long targetId, Long receiverId) {
+                        Long senderId, Long targetId, Long receiverId) {
         this.type = type;
-        this.title = title;
         this.content = content;
         this.senderId = senderId;
-        this.senderName = senderName;
-        this.senderProfileImage = senderProfileImage;
         this.targetId = targetId;
         this.receiverId = receiverId;
         this.read = false;
