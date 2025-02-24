4afecc281e7eba438ddbeadacca7fa2a3cca4869
soyeon <ksy1118s@naver.com>
2025-02-24 09:51:20 +0900

    Fix: 생성자 추가

--- roome/src/main/java/com/roome/domain/room/dto/UpdateRoomThemeRequestDto.java
+++ roome/src/main/java/com/roome/domain/room/dto/UpdateRoomThemeRequestDto.java
@@ -1,10 +1,12 @@
 package com.roome.domain.room.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class UpdateRoomThemeRequestDto {
     private String themeName;
 }
