8c9c57582013a77b7ff50503eca37ec8acbeca00
exxyeon <leehayeon200@naver.com>
2025-02-27 12:20:52 +0900

    Fix: SecurityConfig에서 리디렉션 설정을 application.yml과 같게 만듦

--- roome/src/main/java/com/roome/global/config/SecurityConfig.java
+++ roome/src/main/java/com/roome/global/config/SecurityConfig.java
@@ -52,7 +52,7 @@ public class SecurityConfig {
                 .baseUri("/oauth2/authorization")
             )
             .redirectionEndpoint(endpoint -> endpoint
-                .baseUri("/oauth/callback/*")
+                .baseUri("/oauth/callback/{registrationId}")
             )
             .userInfoEndpoint(endpoint -> endpoint
                 .userService(oAuth2UserService)
