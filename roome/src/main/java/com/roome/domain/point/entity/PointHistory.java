4390cdfe258a892ffd7360a8e77e21b28379e367
exxyeon <leehayeon200@naver.com>
2025-02-27 14:08:32 +0900

    Fix: point_history 테이블에서 user 엔티티와의 관계 설정 변경

--- roome/src/main/java/com/roome/domain/point/entity/PointHistory.java
+++ roome/src/main/java/com/roome/domain/point/entity/PointHistory.java
@@ -1,10 +1,22 @@
 package com.roome.domain.point.entity;
 
 import com.roome.domain.user.entity.User;
-import jakarta.persistence.*;
-import lombok.*;
-
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Entity
@@ -16,7 +28,7 @@ public class PointHistory {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  @OneToOne(fetch = FetchType.LAZY)
+  @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "user_id")
   private User user;
 
