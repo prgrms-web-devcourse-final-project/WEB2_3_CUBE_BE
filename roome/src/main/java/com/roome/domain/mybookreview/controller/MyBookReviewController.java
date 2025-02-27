2a5cc8c34c7cf10c5132f2e02f9cd11c36b30fae
daeyoung <pstor23@gmail.com>
2025-02-27 18:26:06 +0900

    Docs: 서평 API 스웨거 호출

--- roome/src/main/java/com/roome/domain/mybookreview/controller/MyBookReviewController.java
+++ roome/src/main/java/com/roome/domain/mybookreview/controller/MyBookReviewController.java
@@ -4,17 +4,21 @@ import com.roome.domain.mybookreview.service.MyBookReviewService;
 import com.roome.domain.mybookreview.service.request.MyBookReviewCreateRequest;
 import com.roome.domain.mybookreview.service.request.MyBookReviewUpdateRequest;
 import com.roome.domain.mybookreview.service.response.MyBookReviewResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "서평 API", description = "서평 등록/조회/수정/삭제 API")
 @RestController
 @RequiredArgsConstructor
 public class MyBookReviewController {
 
     private final MyBookReviewService myBookReviewService;
 
+    @Operation(summary = "서평 등록", description = "서평을 등록할 수 있다.")
     @PostMapping("/api/mybooks-review")
     public ResponseEntity<MyBookReviewResponse> create(
             @AuthenticationPrincipal Long loginUserId,
@@ -25,12 +29,14 @@ public class MyBookReviewController {
         return ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "서평 조회", description = "서평을 조회할 수 있다.")
     @GetMapping("/api/mybooks-review")
     public ResponseEntity<MyBookReviewResponse> read(@RequestParam("myBookId") Long myBookId) {
         MyBookReviewResponse response = myBookReviewService.read(myBookId);
         return ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "서평 수정", description = "서평을 수정할 수 있다.")
     @PatchMapping("/api/mybooks-review/{myBookReviewId}")
     public ResponseEntity<MyBookReviewResponse> update(
             @AuthenticationPrincipal Long loginUserId,
@@ -41,6 +47,7 @@ public class MyBookReviewController {
         return ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "서평 삭제", description = "서평을 삭제할 수 있다.")
     @DeleteMapping("/api/mybooks-review/{myBookReviewId}")
     public ResponseEntity<Void> delete(
             @AuthenticationPrincipal Long loginUserId,
