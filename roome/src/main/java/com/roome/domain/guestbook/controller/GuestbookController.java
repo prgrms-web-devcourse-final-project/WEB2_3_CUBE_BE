475f68be483a958d33867ede66832ded5b0e6b57
soyeon1806 <ksy1118s@naver.com>
2025-02-28 12:20:55 +0900

    Refactor: 스웨거 태그 추가

--- roome/src/main/java/com/roome/domain/guestbook/controller/GuestbookController.java
+++ roome/src/main/java/com/roome/domain/guestbook/controller/GuestbookController.java
@@ -2,11 +2,14 @@ package com.roome.domain.guestbook.controller;
 
 import com.roome.domain.guestbook.dto.*;
 import com.roome.domain.guestbook.service.GuestbookService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "Guestbook API", description = "방명록 관련 API")
 @RestController
 @RequestMapping("/api/guestbooks")
 @RequiredArgsConstructor
@@ -14,6 +17,7 @@ public class GuestbookController {
 
     private final GuestbookService guestbookService;
 
+    @Operation(summary = "방명록 조회", description = "주어진 방 ID에 대한 방명록을 페이지별로 조회")
     @GetMapping("/{roomId}")
     public ResponseEntity<GuestbookListResponseDto> getGuestbook(
             @PathVariable Long roomId,
@@ -22,6 +26,7 @@ public class GuestbookController {
         return ResponseEntity.ok(guestbookService.getGuestbook(roomId, page, size));
     }
 
+    @Operation(summary = "방명록 추가", description = "주어진 방 ID에 방명록 추가")
     @PostMapping("/{roomId}")
     public ResponseEntity<GuestbookResponseDto> addGuestbook(
             @PathVariable Long roomId,
@@ -31,6 +36,7 @@ public class GuestbookController {
         return ResponseEntity.ok(guestbookService.addGuestbook(roomId, userId, requestDto));
     }
 
+    @Operation(summary = "방명록 삭제", description = "주어진 방명록 ID에 해당하는 방명록 삭제")
     @DeleteMapping("/{guestbookId}")
     public ResponseEntity<Void> deleteGuestbook(
             @PathVariable Long guestbookId,
