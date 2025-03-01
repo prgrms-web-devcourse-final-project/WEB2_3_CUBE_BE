511301b2866dd4c607afc7bc64c1624e32fcaec8
exxyeon <leehayeon200@naver.com>
2025-03-01 21:36:51 +0900

    Feat: provider id로 사용자 조회 메서드 추가

--- roome/src/main/java/com/roome/domain/user/repository/UserRepository.java
+++ roome/src/main/java/com/roome/domain/user/repository/UserRepository.java
@@ -1,38 +1,38 @@
 package com.roome.domain.user.repository;
 
+import com.roome.domain.user.entity.Provider;
 import com.roome.domain.user.entity.User;
 import com.roome.global.jwt.exception.UserNotFoundException;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Optional;
-
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    default User getById(Long id) {
-        return findById(id)
-                .orElseThrow(UserNotFoundException::new);
-    }
+  default User getById(Long id) {
+    return findById(id)
+        .orElseThrow(UserNotFoundException::new);
+  }
 
-    Optional<User> findByProviderId(String providerId);
+  Optional<User> findByProviderAndProviderId(Provider provider, String providerId);
 
-    Optional<User> findByEmail(String email);
+  Optional<User> findByEmail(String email);
 
-    @Transactional
-    @Modifying(clearAutomatically = true)
-    @Query(
-            value = """
-                update users set last_login = :now where id = :userId
-                """,
-            nativeQuery = true
-    )
-    void updateLastLogin(Long userId, LocalDateTime now);
+  @Transactional
+  @Modifying(clearAutomatically = true)
+  @Query(
+      value = """
+          update users set last_login = :now where id = :userId
+          """,
+      nativeQuery = true
+  )
+  void updateLastLogin(Long userId, LocalDateTime now);
 
-    List<User> findByIdNot(Long id);
+  List<User> findByIdNot(Long id);
 }
