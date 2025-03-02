40acb0b8a9b0e4dc8123556854094106d5ae89d8
daeyoung <pstor23@gmail.com>
2025-03-02 22:24:19 +0900

    docs: 가구 스웨거 설정

--- roome/src/main/java/com/roome/domain/furniture/controller/FurnitureController.java
+++ roome/src/main/java/com/roome/domain/furniture/controller/FurnitureController.java
@@ -1,6 +1,8 @@
 package com.roome.domain.furniture.controller;
 
 import com.roome.domain.furniture.service.FurnitureService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -9,12 +11,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "가구 API", description = "가구 레벨 업그레이드")
 @RestController
 @RequiredArgsConstructor
 public class FurnitureController {
 
     private final FurnitureService furnitureService;
 
+    @Operation(summary = "책장 레벨 업그레이드", description = "책장 레벨을 업그레이드 할 수 있다.")
     @PostMapping("/api/rooms/{roomId}/furniture/bookshelf")
     public ResponseEntity<Void> upgradeBookshelf(
             @AuthenticationPrincipal Long loginUserId,
