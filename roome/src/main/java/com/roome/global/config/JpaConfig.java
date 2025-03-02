02f9e5646c4639d7dc77edc3caaf9df5d4c00dfb
chesthyeon <niw927@naver.com>
2025-03-03 01:08:26 +0900

    feat : 엔티티 생성시에 자동으로 생성 시간을 추가할 수 있도록 수정

--- roome/src/main/java/com/roome/global/config/JpaConfig.java
+++ roome/src/main/java/com/roome/global/config/JpaConfig.java
@@ -1,8 +1,10 @@
 package com.roome.global.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
+@EnableJpaAuditing
 public class JpaConfig {
 
 }
