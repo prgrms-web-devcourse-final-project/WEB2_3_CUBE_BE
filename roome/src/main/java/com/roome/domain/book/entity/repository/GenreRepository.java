8d0e0523ec69d7a11c01122541b9e757651c880a
soyeon <ksy1118s@naver.com>
2025-03-03 21:17:10 +0900

    Fix: 가구가 응답될 때 top3 book 장르와 cd 장르도 함께 응답되도록 수정

--- roome/src/main/java/com/roome/domain/book/entity/repository/GenreRepository.java
+++ roome/src/main/java/com/roome/domain/book/entity/repository/GenreRepository.java
@@ -2,10 +2,16 @@ package com.roome.domain.book.entity.repository;
 
 import com.roome.domain.book.entity.Genre;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface GenreRepository extends JpaRepository<Genre, Long> {
 
     Optional<Genre> findByName(String name);
+
+    @Query("SELECT bg.genre.name FROM BookGenre bg WHERE bg.book.id IN " +
+            "(SELECT mb.book.id FROM MyBook mb WHERE mb.room.id = :roomId)")
+    List<String> findGenresByRoomId(Long roomId);
 }
