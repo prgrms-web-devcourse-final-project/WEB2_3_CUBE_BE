9877594ef0aa1974c5af1b7a7d1e8adae25d51af
soyeon <ksy1118s@naver.com>
2025-03-03 22:02:41 +0900

    Feat: 테마 변경 전, 잠긴 테마면 구매 로직 추가

---
+++ roome/src/main/java/com/roome/domain/room/repository/RoomThemeUnlockRepository.java
@@ -0,0 +1,11 @@
+package com.roome.domain.room.repository;
+
+import com.roome.domain.room.entity.RoomTheme;
+import com.roome.domain.room.entity.RoomThemeUnlock;
+import com.roome.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface RoomThemeUnlockRepository extends JpaRepository<RoomThemeUnlock, Long> {
+    boolean existsByUserAndTheme(User user, RoomTheme theme);
+}
