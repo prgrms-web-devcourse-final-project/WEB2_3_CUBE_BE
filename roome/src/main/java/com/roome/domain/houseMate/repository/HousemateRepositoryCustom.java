eb558c6e58313c3e71de1eeb8cfd880109091d41
chesthyeon <niw927@naver.com>
2025-03-04 12:52:51 +0900

    refactor : HousemateRepositoryCustom.java, HousemateRepositoryImpl.java 변경  실시간 알림을 나와 관련된 사용자에게만 알림을 보내기 위해

--- roome/src/main/java/com/roome/domain/houseMate/repository/HousemateRepositoryCustom.java
+++ roome/src/main/java/com/roome/domain/houseMate/repository/HousemateRepositoryCustom.java
@@ -7,4 +7,6 @@ import java.util.List;
 public interface HousemateRepositoryCustom {
     List<HousemateInfo> findByUserId(Long userId, Long cursor, int limit, String nickname);
     List<HousemateInfo> findByAddedId(Long addedId, Long cursor, int limit, String nickname);
+    List<Long> findFollowerIdsByAddedId(Long addedId);
+    List<Long> findFollowingIdsByUserId(Long userId);
 }
