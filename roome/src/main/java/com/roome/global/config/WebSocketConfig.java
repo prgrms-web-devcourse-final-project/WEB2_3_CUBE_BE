619484d364fe425e4d2b37bbf86f7edd5b73596a
chesthyeon <niw927@naver.com>
2025-03-05 11:16:22 +0900

    refactor : WebSocketConfig.java 수정

--- roome/src/main/java/com/roome/global/config/WebSocketConfig.java
+++ roome/src/main/java/com/roome/global/config/WebSocketConfig.java
@@ -3,9 +3,12 @@ package com.roome.global.config;
 import com.roome.global.jwt.interceptor.JwtWebSocketInterceptor;
 import com.roome.global.jwt.service.JwtTokenProvider;
 import com.roome.global.service.RedisService;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -29,6 +32,15 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
                 .setHeartbeatValue(new long[]{25000, 25000}); // 서버->클라이언트, 클라이언트->서버 하트비트 주기 설정(밀리초 단위)
         config.setUserDestinationPrefix("/user");
         config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Bean
+    public TaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(1);
+        scheduler.setThreadNamePrefix("websocket-heartbeat-");
+        scheduler.initialize();
+        return scheduler;
     }
 
     @Override
