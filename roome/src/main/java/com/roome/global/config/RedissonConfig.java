4acb01f2e88bd9f709d14c9218f73d12dca7f45e
chesthyeon <niw927@naver.com>
2025-03-10 01:39:42 +0900

    refactor: RedissonConfig.java 레디스 비밀번호 설정 추가.

--- roome/src/main/java/com/roome/global/config/RedissonConfig.java
+++ roome/src/main/java/com/roome/global/config/RedissonConfig.java
@@ -16,11 +16,15 @@ public class RedissonConfig {
   @Value("${spring.data.redis.port}")
   private int redisPort;
 
+  @Value("${spring.data.redis.password}")
+  private String redisPassword;
+
   @Bean
   public RedissonClient redissonClient() {
     Config config = new Config();
     config.useSingleServer()
-        .setAddress("redis://" + redisHost + ":" + redisPort);
+            .setAddress("redis://" + redisHost + ":" + redisPort)
+            .setPassword(redisPassword);  // 비밀번호 설정 추가
     return Redisson.create(config);
   }
 }
