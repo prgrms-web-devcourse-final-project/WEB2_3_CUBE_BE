8b2dc791cabdfa1a9d26235c4d143a21e6ad049a
jhyngu <154207428+jhyngu@users.noreply.github.com>
2025-03-09 15:01:15 +0900

    Feat: CdTemplate 중복 체크 후 저장 로직 추가 (#367)
    * feat: CdTemplate 중복 체크 후 저장 로직 추가
    
    - `existsByMyCdId(myCdId)`를 사용하여 중복 체크 후 저장
    - 동일한 `myCdId`가 존재하면 `DuplicateCdTemplateException` 발생
    - 중복 데이터 방지 및 안정적인 템플릿 생성 로직 유지
    
    * chore: MyBookReviewControllerTest 테스트 코드 주석 처리

---
+++ roome/src/main/java/com/roome/domain/cdtemplate/exception/DuplicateCdTemplateException.java
@@ -0,0 +1,8 @@
+package com.roome.domain.cdtemplate.exception;
+
+public class DuplicateCdTemplateException extends RuntimeException {
+
+  public DuplicateCdTemplateException(String message) {
+    super(message);
+  }
+}
