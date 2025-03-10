6b3510891d3646e8b96ca1a89612343aed927b75
exxyeon <leehayeon200@naver.com>
2025-03-10 15:45:47 +0900

    feat: 회원 탈퇴 시 결제 데이터(Payment, PaymentLog) 연관 삭제 기능 추가 (#371)

--- roome/src/main/java/com/roome/domain/payment/repository/PaymentLogRepository.java
+++ roome/src/main/java/com/roome/domain/payment/repository/PaymentLogRepository.java
@@ -2,19 +2,21 @@ package com.roome.domain.payment.repository;
 
 import com.roome.domain.payment.entity.PaymentLog;
 import com.roome.domain.user.entity.User;
+import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface PaymentLogRepository extends JpaRepository<PaymentLog, Long> {
 
-    // 특정 사용자(userId)의 결제 기록 조회
-    List<PaymentLog> findByUserId(Long userId);
+  // 특정 사용자(userId)의 결제 기록 조회
+  List<PaymentLog> findByUserId(Long userId);
 
-    Page<PaymentLog> findByUser(User user, Pageable pageable);
+  Page<PaymentLog> findByUser(User user, Pageable pageable);
 
-    // 특정 결제 키(paymentKey)로 결제 내역 조회
-    List<PaymentLog> findByPaymentKey(String paymentKey);
+  // 특정 결제 키(paymentKey)로 결제 내역 조회
+  List<PaymentLog> findByPaymentKey(String paymentKey);
+
+  // 특정 사용자(userId)의 결제 로그 삭제
+  void deleteByUserId(Long userId);
 }
